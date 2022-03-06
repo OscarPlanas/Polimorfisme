c7119a6cc12dc746e627e5cfea32c20c4a8084c0
OscarPlanas <casplanas7@gmail.com>
2022-03-06 12:09:10 +0100

    segundo commit

---
+++ src/Square.java
@@ -0,0 +1,15 @@
+package edu.upc.eetac.dsa;
+
+public class Cuadrado extends Rectangulo {//Hereda de rectangulo, por lo tanto debe contener los mismo atributos que su padre
+
+    public Cuadrado(double l1, double l2) {
+
+        super(l1, l2);
+    }
+
+    @Override
+    public double area() {
+
+        return super.area();
+    }
+}
